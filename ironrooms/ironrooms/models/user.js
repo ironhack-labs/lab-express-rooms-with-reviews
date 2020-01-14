cb13fb8bd76c435ef4664bcf342d26e595b75255
Anastasia Schmitke <schmitke@mediamarktsaturn.com>
2020-01-14 11:46:59 +0100

    Iteration 1 finished

---
+++ ironrooms/ironrooms/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: String,
+    password: String,
+    fullName: String,
+    // slack login - optional
+    slackID: String,
+    // google login - optional
+    googleID: String
+  }, {
+    timestamps: true
+  });
+
+  const User = mongoose.model("User", userSchema);
+
+
+  module.exports = User;
