e6eeb0d5dd2349cc92e24a8429acab9e1872515c
jesuscabrera <chuscabrera@gmail.com>
2020-05-03 16:32:41 +0200

    Iteration 2 - done

--- rooms-app/routes/rooms.js
+++ rooms-app/routes/rooms.js
@@ -8,13 +8,16 @@ router.get("/add", (req, res) => {
 
 router.get("/", (req, res, next) => {
   // show all rooms
-  Room.find()
+  Room.find({})
+    // populate to access the name from the user in the referenced Mongo DB table
+    .populate("owner", "fullName")
     .then((rooms) => {
       res.render("rooms/index", { roomsList: rooms });
     })
     .catch((err) => {
       next(err);
     });
+
   // just show the rooms of the logged in user
   /*
   Room.find({ owner: req.user._id })
@@ -49,29 +52,58 @@ router.post("/", (req, res, next) => {
     });
 });
 
-// deletes the room
-// an admin can delete any room - a user can only
-// delete it when she is the owner
 router.get("/:roomId/", (req, res, next) => {
-  const query = { _id: req.params.roomId };
-
-  if (req.user.role !== "admin") {
-    query.owner = req.user._id;
-  }
-
-  // that is the long version of what is happening
-  // if user.role !== 'admin'
-  // query: { _id: req.params.roomId, owner: req.user._id }
-  // else if user.role === 'admin'
-  // query; { _id: req.params.roomId }
-
-  Room.findOneAndDelete(query)
-    .then(() => {
-      res.redirect("/rooms");
+  // show detail Room
+  const roomQuery = { _id: req.params.roomId };
+  Room.find(roomQuery)
+    // populate to access the name from the user in the referenced Mongo DB table
+    .populate("owner", "fullName")
+    .then((room) => {
+      res.render("rooms/roomDetails", { room });
     })
     .catch((err) => {
       next(err);
     });
 });
+
+// deletes the room
+// an admin can delete any room - a user can only
+// delete it when she is the owner
+
+router.get("/delete/:roomId/", (req, res, next) => {
+  const roomQueryDelete = { _id: req.params.roomId };
+
+  if (req.isAuthenticated()) {
+    Room.findOneAndDelete(roomQueryDelete)
+      .then(() => {
+        res.redirect("/rooms");
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+});
+
+// router.get("/:roomId/", (req, res, next) => {
+//   const query = { _id: req.params.roomId };
+
+//   if (req.user.role !== "admin") {
+//     query.owner = req.user._id;
+//   }
+
+// that is the long version of what is happening
+// if user.role !== 'admin'
+// query: { _id: req.params.roomId, owner: req.user._id }
+// else if user.role === 'admin'
+// query; { _id: req.params.roomId }
+
+// Room.findOneAndDelete(query)
+//   .then(() => {
+//     res.redirect("/rooms");
+//   })
+//   .catch((err) => {
+//     next(err);
+//   });
+// });
 
 module.exports = router;
