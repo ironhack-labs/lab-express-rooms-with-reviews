a2df01b8ae522371c63498c8056e8719c456265a
Israel Zambrana <zambrana90@gmail.com>
2021-07-03 12:16:43 +0200

    iteration 1

---
+++ config/routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const miscController = require('../controllers/misc.controller');
+const authController = require('../controllers/auth.controller');
+
+router.get('/', miscController.index);
+
+// Register
+router.get('/register', authController.register);
+router.post('/register', authController.doRegister);
+
+// Login
+router.get('/login', authController.login);
+router.post('/login', authController.doLogin);
+
+module.exports = router;
