8f8088600d6adb2254eb4dcddec1c13ae17372bb
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-08 18:50:11 +0200

    routes

--- rooms-app/app.js
+++ rooms-app/app.js
@@ -1,34 +1,45 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+// ℹ️ This function is getting exported from the config folder. It runs most middlewares
+require('./config')(app);
 
 // default value for title local
-const projectName = "rooms-app";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = 'lab-express-basic-auth';
+const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require("./routes/index");
-app.use("/", index);
+const index = require('./routes/index');
+const authRouter = require('./routes/auth.routes');
+// Private router
+const privateRouter = require('./routes/private.routes');
+
+
+app.use('/', index);
+app.use('/auth', authRouter);
+// Set a specific router for protected routes
+app.use('/private', privateRouter);
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
+
