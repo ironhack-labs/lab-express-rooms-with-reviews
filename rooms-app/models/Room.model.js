e583bbd45926d4c1249177525fb5d3aee369466d
Jesse Vermeulen <jessevermeulen123@gmail.com>
2021-09-08 17:12:38 +0200

    iteration 1

---
+++ rooms-app/models/Room.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const roomSchema = new Schema({
+    name: { type: String },
+    description: { type: String },
+    imageUrl: { type: String },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    reviews: [] // we will update this field a bit later when we create review model
+  });
+
+const Room = model("Room", roomSchema);
+
+module.exports = User;
+
+
