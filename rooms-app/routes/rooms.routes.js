6c8bc4602f6438646cc2da4084541b48b9864981
Gabriel-PF <fsp.gabriel@gmailcom>
2022-05-20 22:55:03 +0200

    First iteration

--- rooms-app/routes/rooms.routes.js
+++ rooms-app/routes/rooms.routes.js
@@ -14,3 +14,82 @@ router.get('/', async (req, res, next) => {
       next(error);
     }
   })
+
+
+// Criar / editar / deletar
+
+/*
+   router.get('/create', (req, res, next) => {
+  res.render('rooms/rooms-create');
+})
+
+router.post('/create', async (req, res, next) => {
+  try {
+    const { name, description, imageUrl, owner, reviews } = req.body;
+    await Room.create({
+      name,
+      description,
+      imageUrl,
+      owner,
+      reviews: []
+    });
+
+    res.redirect('/rooms');
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.get('/:id/edit', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const room = await Room.findById(id);
+    res.render('rooms/rooms-edit', room);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.post('/:id/edit', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, description, imageUrl, owner, reviews } = req.body;
+    await Room.findByIdAndUpdate(id,
+      {
+        name,
+      description,
+      imageUrl,
+      owner,
+      reviews: []
+      },
+      {
+        new: true
+      });
+    
+      res.redirect(`/rooms/${id}`);
+  } catch (error) {
+    next(error);
+  }
+})
+
+router.post('/:id/delete', async (req, res, next) => {
+  try {
+      const { id } = req.params;
+      await Room.findByIdAndDelete(id);
+
+      res.redirect('/rooms');
+  }catch(error) {
+      next(error);
+  }
+})
+
+router.get("/rooms", async (req, res, next) => {
+  try {
+    const rooms = await Room.find().populate('owner').populate('reviews');
+    res.render("rooms/rooms-list", { rooms });
+  } catch(error) {
+    next(error);
+  }
+});
+
+module.exports = router;  /*
