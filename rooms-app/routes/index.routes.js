6006a211b9d9ec4274f230269379e9b559538b9e
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-05-21 13:31:53 +0200

    app.js - index links

--- rooms-app/routes/index.routes.js
+++ rooms-app/routes/index.routes.js
@@ -5,12 +5,7 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-<<<<<<< HEAD
 router.use('/rooms', require('./rooms.routes'));
 router.use('/auth', require('./auth.routes'));  
-=======
-//router.use('/rooms', require('./rooms.routes'));
-//router.use('/auth', require('./auth.routes')); 
->>>>>>> 57aa174112f55541e1b40c8800c15c51b84ceb6d
 
 module.exports = router;
