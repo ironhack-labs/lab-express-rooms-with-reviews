c491ade836ab148d4f5ce2a0a30412c04c9d7adb
Ornella Escalada Corsi <ornellaescaladacorsi@Ornellas-Air.fritz.box>
2022-05-21 11:05:45 +0200

    signin

--- rooms-app/routes/auth.routes.js
+++ rooms-app/routes/auth.routes.js
@@ -1,81 +1,83 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const bcrypt = require("bcryptjs");
-const User = require('../models/User.model');
+const User = require("../models/User.model");
 
 router.get("/logout", (req, res) => {
-	req.session.destroy((error) => {
-		if (error) {
-            const errorMessage = error.message;
-            res.render("auth/logout", {errorMessage});
-            return
-		}
-		res.redirect("/");
-	})
+  req.session.destroy((error) => {
+    if (error) {
+      const errorMessage = error.message;
+      res.render("auth/logout", { errorMessage });
+      return;
+    }
+    res.redirect("/");
+  });
 });
 
 const displaySignup = (req, res) => res.render("auth/signup");
 router.get("/signup", displaySignup);
 
 router.post("/signup", async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if(!password || !email) {
-        const errorMessage = 'Your password or username are not valid';
-        res.render("auth/signup", {errorMessage});
-        return
+  if (!password || !email) {
+    const errorMessage = "Your password or username are not valid";
+    res.render("auth/signup", { errorMessage });
+    return;
+  }
+  try {
+    const foundUser = await User.findOne({ email });
+    if (foundUser) {
+      const errorMessage = "You are already registered!";
+      res.render("auth/signup", { errorMessage });
+      return;
     }
-    try {
-      const foundUser = await User.findOne({ email }); 
-        if (foundUser) {
-            const errorMessage = 'You are already registered!'
-            res.render("auth/signup", { errorMessage });
-            return
-        }
-        const hashedPassword = bcrypt.hashSync(password, 12);
-        const createdUser = await User.create({
-            email,
-            password: hashedPassword
-        })
-        
-        res.redirect("/signin");
-    } catch(error){
-        next(error);
-    }
+    const hashedPassword = bcrypt.hashSync(password, 12);
+    const createdUser = await User.create({
+      email,
+      password: hashedPassword,
+    });
+
+    res.redirect("/signin");
+  } catch (error) {
+    next(error);
+  }
 });
 router.get("/signin", (req, res) => {
-    res.render("auth/signin")
-})
-router.post("/signin", async (req, res, next)=>{
-    const { email, password } = req.body
+  res.render("auth/signin");
+});
+router.post("/signin", async (req, res, next) => {
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-		return res.render("auth/signin", {
-			errorMessage: "Please provide an email and a a password",
-		})
-	}
-    try {
-		const foundUser = await User.findOne({ email })
+  if (!email || !password) {
+    res.render("auth/signin", {
+      errorMessage: "Please provide an email and a a password",
+    });
+  }
+  try {
+    const foundUser = await User.findOne({ email });
 
-		if (!foundUser) {
-			return res.render("auth/signin", {
-				errorMessage: "Wrong credentials",
-			})
-		}
+    if (!foundUser) {
+      res.render("auth/signin", {
+        errorMessage: "Wrong credentials",
+      });
+    }
 
-		const checkPassword = bcrypt.compareSync(password, foundUser.password)
-		if (!checkPassword) {
-			res.render("auth/signin", {
-				errorMessage: "Wrong credentials",
-			})
-		}
-		const objectUser = foundUser.toObject()
-		delete objectUser.password
-		req.session.currentUser = objectUser
-
-		return res.redirect("/")
-	} catch (e) {
-		next(e)
-	}
-})
+    const checkPassword = bcrypt.compareSync(password, foundUser.password);
+    console.log(checkPassword);
+    if (!checkPassword) {
+      res.render("auth/signin", {
+        errorMessage: "Wrong credentials",
+      });
+    } else {
+      const objectUser = foundUser.toObject();
+      delete objectUser.password;
+      req.session.currentUser = objectUser;
+      console.log(objectUser);
+      res.redirect("/");
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
