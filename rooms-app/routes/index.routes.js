5cebf07fecaa5b167fa3728a53ced36cc1651c9e
Ornella-e <orneescaladacorsi@gmail.com>
2022-05-23 18:07:24 +0200

    update

--- rooms-app/routes/index.routes.js
+++ rooms-app/routes/index.routes.js
@@ -7,5 +7,6 @@ router.get("/", (req, res, next) => {
 
 router.use("/", require("./auth.routes"));
 router.use("/", require ("./rooms.routes"));
+router.use("/", require ("./reviews.routes"));
 
 module.exports = router;
--- rooms-app/routes/reviews.routes.js
+++ rooms-app/routes/reviews.routes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
 const Review = require("../models/Reviews.model");
+const isLoggedIn = require("../middlewares/isLoggedIn.js");
 
-router.get("/", (req, res) =>{
-   res.render("comments")
+router.get("/rooms", isLoggedIn, (req, res) =>{
+   res.render("auth/comments")
 });
 
 module.exports = router
