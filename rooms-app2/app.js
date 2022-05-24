fff16f4f6231cce010600e8172681c6b630ba456
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-05-24 21:09:30 +0200

    last commit

--- rooms-app2/app.js
+++ rooms-app2/app.js
@@ -24,11 +24,9 @@ const projectName = "rooms-app2";
 app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
+
 const index = require("./routes/index.routes");
 app.use("/", index);
-
-const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
