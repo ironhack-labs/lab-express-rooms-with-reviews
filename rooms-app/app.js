bc02f67a1c64e42036ba40e7131a2e9fcdbe7f8a
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-06-28 12:28:18 +0200

    basic done

---
+++ rooms-app/app.js
@@ -0,0 +1,52 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv/config");
+
+// ℹ️ Connects to the database
+require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require("hbs");
+
+const app = express();
+//     |
+//     |-----------------------------|
+// use session here:                 V
+require("./config/session.config")(app);
+//                                  ^
+//                                  |
+// the "app" that gets passed here is the
+// previously defined Express app (const app = express();)
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
+
+// default value for title local
+const capitalized = require("./utils/capitalized");
+const projectName = "rooms-app";
+
+app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`;
+
+// 👇 Start handling routes here
+const index = require("./routes/index.routes");
+app.use("/", index);
+
+// authRouter needs to be added so paste the following lines:
+const authRouter = require("./routes/auth.routes"); // <== has to be added
+app.use("/", authRouter); // <== has to be added
+
+const mainRoutes = require("./routes/main.routes");
+app.use("/", mainRoutes);
+
+const privateRoutes = require("./routes/private.routes");
+app.use("/", privateRoutes);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+module.exports = app;
---
+++ rooms-app/routes/auth.routes.js
@@ -0,0 +1,133 @@
+const { Router } = require("express");
+const router = new Router();
+
+const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
+
+// require auth middleware
+const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
+
+const User = require("../models/User.model");
+
+// GET route ==> to display the signup form to users
+router.get("/signup", isLoggedOut, (req, res) => res.render("auth/signup"));
+
+// POST route ==> to process form data
+router.post("/signup", isLoggedOut, (req, res, next) => {
+  // console.log("The form data: ", req.body);
+
+  const { username, email, password } = req.body;
+
+  // make sure users fill all mandatory fields:
+  if (!username || !email || !password) {
+    res.render("auth/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
+    return;
+  }
+
+  // make sure passwords are strong:
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        // username: username
+        username,
+        email,
+        // passwordHash => this is the key from the User model
+        //     ^
+        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+        passwordHash: hashedPassword,
+      });
+    })
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      res.redirect("/userProfile");
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render("auth/signup", {
+          errorMessage:
+            "Username and email need to be unique. Either username or email is already used.",
+        });
+      } else {
+        next(error);
+      }
+    });
+});
+
+//////////// L O G I N ///////////
+
+// GET route ==> to display the login form to users
+router.get("/login", isLoggedOut, (req, res) => res.render("auth/login"));
+
+// POST login route ==> to process form data
+router.post("/login", isLoggedOut, (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, email and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ email }) // <== check if there's user with the provided email
+    .then((user) => {
+      // <== "user" here is just a placeholder and represents the response from the DB
+      if (!user) {
+        // <== if there's no user with provided email, notify the user who is trying to login
+        res.render("auth/login", {
+          errorMessage: "Email is not registered. Try with other email.",
+        });
+        return;
+      }
+      // if there's a user, compare provided password
+      // with the hashed password saved in the database
+      else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // if the two passwords match, render the user-profile.hbs and
+        //                   pass the user object to this view
+        //                                 |
+        //                                 V
+        // res.render("users/user-profile", { user });
+
+        // when we introduce session, the following line gets replaced with what follows:
+        // res.render('users/user-profile', { user });
+
+        //******* SAVE THE USER IN THE SESSION ********//
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        // if the two passwords DON'T match, render the login form again
+        // and send the error message to the user
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+router.get("/userProfile", isLoggedIn, (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+router.post("/logout", isLoggedIn, (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
+module.exports = router;
