efe370bc1f45e131e3f3eebdeed4ca74f54b40ec
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-06-30 16:39:17 +0200

    editing room model

--- rooms-app/models/Room.model.js
+++ rooms-app/models/Room.model.js
@@ -3,12 +3,9 @@ const { Schema, model } = require("mongoose");
 const roomSchema = new Schema({
   name: { type: String },
   description: { type: String },
-  imageUrl: { 
-    type: String, 
-    required: [true, "imageUrl is required."], 
-},
+  imageUrl: { type: String },
   owner: { type: Schema.Types.ObjectId, ref: "User" },
-  // reviews: [] // we will update this field a bit later when we create review model
+  reviews: [] // we will update this field a bit later when we create review model
   // reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }];
 });
 
