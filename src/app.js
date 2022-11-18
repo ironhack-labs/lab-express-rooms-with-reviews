51b07ab59a782c8094cc243f1a0f3de7caaeb7cb
amandahp <amandahoffmann96@gmail.com>
2022-11-17 23:25:28 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes.js";
 import connectToMongoose from "./config/database.js";
 
@@ -12,6 +13,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
